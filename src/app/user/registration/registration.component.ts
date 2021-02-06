8361a87c1093c3b44d4661a52687b8b09357d01b
misha <misha@bk.com>
2021-02-06 23:43:53 +0300

    setup registration component

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { UserService } from '../../shared/user.service';
 
 @Component({
@@ -8,17 +9,42 @@ import { UserService } from '../../shared/user.service';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor(public sevrice: UserService) { }
+  constructor(public sevrice: UserService, private toastr: ToastrService) { }
+
+// toastr not working,хз почему
+// TODO: fix toastr
 
   ngOnInit(): void {
+    this.sevrice.form.reset();
   }
 
-  onSubmit(){
-    this.sevrice.register().subscribe(res=>{
-      console.log(res);
-    }, err =>{
-      console.log(err);
-    })
+  onSubmit() {
+    this.sevrice.register().subscribe(
+      (res: any) => {
+        if (res.succeded) {
+          this.sevrice.form.reset();
+          
+          this.toastr.success('New user created', 'Registration successful.');
+          alert('New user created');
+        }
+      },
+      (err: any) => {
+        console.log(err);
+        err.error.forEach(element => {
+          console.log(element);
+          switch (element.code) {
+            case "DuplicateUserName":
+              this.toastr.error('User name is already taken', 'Registration failde.');
+              alert('User name is already taken');
+              break;
+            default:
+              this.toastr.error(`${element.description}`, 'Registration failde.');
+              alert(`${element.description}`);
+              break;
+          }
+        }
+        )
+      }
+    )
   }
-
 }
