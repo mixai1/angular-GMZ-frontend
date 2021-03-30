47e50c0c3e32940e25e3e9299d1f69b90fc08b33
misha <kachan_mixail@bk.ru>
2021-03-30 16:05:53 +0300

    add UserProf component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,16 +9,18 @@ import { NewsComponent } from './news/news.component';
 import { ContactComponent } from './contact/contact.component';
 import { HistoryComponent } from './history/history.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'user/registr', component: RegistrationComponent },
   { path: 'user/login', component: LoginComponent },
   { path: 'product', component: ProductComponent },
   { path: 'news', component: NewsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'history', component: HistoryComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent },
+  { path: 'userProf', component: UserProfileComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent }
 ];
 
---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+  constructor(private roter:Router) { }
+
+  ngOnInit(): void {
+  }
+  onlogout(){
+    localStorage.removeItem('token');
+    this.roter.navigateByUrl('user/login');
+  }
+
+}
