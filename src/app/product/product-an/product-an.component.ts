e5607b4961d09f0f538dfcd6e09e18f84b9744c0
misha <kachan_mixail@bk.ru>
2021-04-08 16:15:36 +0300

    add some-product item

---
+++ src/app/product/product-an/product-an.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-an',
+  templateUrl: './product-an.component.html',
+  styleUrls: ['./product-an.component.css']
+})
+export class ProductANComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
+import { ProductAPPComponent } from './product-app/product-app.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { ProductANComponent } from './product-an/product-an.component';
+import { ProductASComponent } from './product-as/product-as.component';
+import { ItemProdComponent } from './item-prod/item-prod.component';
 
 @NgModule({
-  declarations: [ProductComponent],
-  imports: [CommonModule]
+  declarations:
+    [
+      ProductComponent,
+      ProductAPPComponent,
+      ProductANComponent,
+      ProductASComponent,
+      ItemProdComponent
+    ],
+  imports: [CommonModule, AppRoutingModule]
 })
 export class ProductModule { }
